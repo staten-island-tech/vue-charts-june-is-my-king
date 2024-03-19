0e593aeaf3d0916adc7a13644359ad6378f1047e
july1012 <113192987+july1012@users.noreply.github.com>
2024-03-19 13:14:40 -0400

    srgsr

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,50 +1,39 @@
 <template>
   <div class="container">
-    <!-- <Pie v-if="loaded" :data="chartData" /> -->
+    <h1>pie chart PSAL by race</h1>
+    <Pie v-if="loaded" :data="chartData" />
   </div>
 </template>
 
 <script>
 // import { Pie } from 'vue-chartjs'
-export default{
-  name:'pie',
-  props:{
+
+export default {
+  name: 'pie',
+  props: {
     msg: String
   },
-  methods:{
-    fetchData(){
-      fetch('https://data.cityofnewyork.us/resource/s6eb-zqd5.json',{
-      method: 'GET',
-    })
-    .then(response =>{
-      response.json(). then (res=>console.log(res));
-    })
-    .catch(err=>{
-      console.error(err);
-    })
 
+mounted: function(){this.fetchdata()},
+  methods: {
+    async fetchdata() {
+      fetch('https://data.cityofnewyork.us/resource/s6eb-zqd5.json', {
+        method: 'GET',
+      })
+        .then(response => {
+          response.json().then(res => console.log(res));
+        })
+        .catch(err => {
+          console.error(err);
+        });
     }
   }
 
-}
+  
+
+};
 
 
-// export default{
-//   let :dataa = [],
-//   const :url= "https://data.cityofnewyork.us/resource/s6eb-zqd5.json",
-
-// async mounted(url) {
-//   try {
-//     const response = await fetch(url);
-//     const dataa = await response.json();
-//     insert(dataa);
-//     console.log("Data:", dataa[0]);
-//     getData(dataa);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
 
 
-// }
-// </script>
+</script>
