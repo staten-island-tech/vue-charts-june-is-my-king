6b79f3cd3353822101ad919d04ab8637189f7b92
july1012 <113192987+july1012@users.noreply.github.com>
2024-03-20 13:15:55 -0400

    ummmm

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,23 +1,53 @@
 <template>
   <div class="container">
-    <h1>pie chart PSAL by race</h1>
+    <h1>pie chart by race</h1>
     <Pie v-if="loaded" :data="chartData" />
   </div>
 </template>
 
 <script>
-// import { Pie } from 'vue-chartjs'
 
+// import { Pie } from 'vue-chartjs'
+export const data = {
+  labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+    }
+  ]
+}
+import PieChart from './PieChart.js'
 export default {
-  name: 'pie',
+  name: 'piechart',
   props: {
-    msg: String
+    components: {
+      PieChart
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    mounted () {
+      this.$nextTick(function () {
+        this.fillData;
+      })
+    },
+    methods: {
+      fillData () {
+        this.$http.get('/api/data/line/scenarios_by_day')
+            .then(response => {
+            this.datacollection = response.data;
+        });
+      },
+    }
   },
+  
 
 mounted: function(){this.fetchdata()},
   methods: {
     async fetchdata() {
-      fetch('https://data.cityofnewyork.us/resource/s6eb-zqd5.json', {
+      fetch('https://data.cityofnewyork.us/resource/ykvb-493p.json?race=Black', {
         method: 'GET',
       })
         .then(response => {
@@ -27,13 +57,17 @@ mounted: function(){this.fetchdata()},
           console.error(err);
         });
     }
-  }
+  },
 
-  
 
 };
 
 
 
 
+
 </script>
+
+<style scoped>
+
+</style>
