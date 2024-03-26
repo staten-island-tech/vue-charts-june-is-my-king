4584321970d4d19051f23d1d8502abcdbc970219
july1012 <113192987+july1012@users.noreply.github.com>
2024-03-26 13:16:32 -0400

    sfsdefcde

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,32 +1,65 @@
 <template>
-  <Pie
+  <Pie v-if="loaded" id="my-chart-id" :data="chartData"/>
 
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+  <canvas id="piechart" width="400" height="400"></canvas>
 </template>
 
 <script>
-
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, PieElement, CategoryScale, LinearScale } from 'chart.js'
+import * as chartConfig from './chartConfig.js'
 
-ChartJS.register(Title, Tooltip, Legend, PieElement, CategoryScale, LinearScale)
+ChartJS.register(ArcElement, Tooltip, Legend)
+export const data = {
+  labels: [],
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      chartData: [40, 20, 80, 10]
+    }
+  ]
+}
 
-export default {
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+
+export default{
   name: 'PieChart',
   components: { Pie },
-  data() {
-    return {
+  data(){
+    return{
+      test:null,
+      loaded: false,
       chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
+        label:[],
+        datasets: [{ data: [7]}]
       }
     }
+  },
+  methods: {
+    getData: async function(){
+      try{
+        const res = await fetch('https://data.cityofnewyork.us/resource/ykvb-493p.json')
+        const data = await res.json()
+        this.test = data
+        data.forEach((dataPoint)=>{
+          this.chartData.labels.push(dataPoint.year)
+        })
+      }catch(e){
+        console.error(e)
+      }
+      
+    } 
+  },
+  mounted:
+  async function(){
+    await this.getData()
+    this.loaded = true
+  },
+  chartData() {
+    return chartConfig
   }
 }
 </script>
