b14d8f2bd69833e26c9a436e83b371640aa182ee
july1012 <113192987+july1012@users.noreply.github.com>
2024-04-01 22:49:46 -0400

    ugh

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,72 +1,68 @@
 <template>
   <div class="container">
-  <Pie v-if="loaded" class="pie" id="my-chart-id" :data="chartData" />
-
- 
-</div>
+    <Pie v-if="loaded" class="pie" id="my-chart-id" :data="chartData" />
+  </div>
 </template>
 
 <script>
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'vue-chartjs'
 
-
 ChartJS.register(ArcElement, Tooltip, Legend)
 
-
-export default{
+export default {
   name: 'PieChart',
   components: { Pie },
   data() {
-    return{
-      
+    return {
       loaded: false,
       chartData: {
-        label:[],
-        datasets: [{ 
-          backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-          data:[]
-        }]
+        label: ['whydfd'],
+        datasets: [
+          {
+            backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+            data: [43, 34, 24]
+          }
+        ]
       }
     }
   },
   methods: {
-    getData: async function(){
-      try{
-        const res = await fetch('https://data.cityofnewyork.us/resource/ykvb-493p.json?borough=All&sex=All&neighborhood=All&year=2021')
+    getData: async function () {
+      try {
+        const res = await fetch(
+          'https://data.cityofnewyork.us/resource/ykvb-493p.json?borough=All&sex=All&neighborhood=All&year=2021'
+        )
         const data = await res.json()
         this.test = data
-        data.forEach((dataPoint)=>{
-          this.chartData.label.push(dataPoint.race)
-        })
-   
-      }catch(e){
+
+        // data.forEach((dataPoint) => {
+        //   this.chartData.label.push(dataPoint.race)
+        // })
+        console.log(this.chartData.label)
+      } catch (e) {
         console.error(e)
       }
-      
-    } 
+    }
   },
-  mounted:
-  async function(){
+  mounted: async function () {
     await this.getData()
     try {
       this.loaded = true
     } catch (error) {
       console.log(error)
     }
-  
-  },
-
+  }
 }
 </script>
 
 <style scoped>
-.pie{
+.pie {
   height: 500px;
   width: 500px;
 }
 
-.container{
+.container {
   display: flex;
   align-items: center;
   flex-wrap: wrap;
