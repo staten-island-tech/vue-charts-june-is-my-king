f814c9e1c70fd3687b4d79b136a646ac99799319
july1012 <113192987+july1012@users.noreply.github.com>
2024-04-02 08:35:13 -0400

    sfsr

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <h2>Cases with over 1000 diagnoses</h2>
+    <h2>Cases with over 1500 diagnoses:</h2>
     <div class="many">
       <h2 v-for="item in cases" :key="item.hiv_diagnoses_num">
         {{ item.year }}: {{ item.hiv_diagnoses_num }} diagnoses
@@ -26,7 +26,7 @@ export default {
         const data = await res.json()
         this.test = data
         data.forEach((point) => {
-          if (point.hiv_diagnoses_num > 1000) {
+          if (point.hiv_diagnoses_num > 1500) {
             this.cases.push(point)
           }
         })
@@ -55,5 +55,7 @@ export default {
   flex-wrap: wrap;
   justify-content: space-around;
   text-align: center;
+  font-size: 20px;
+  margin: 2rem;
 }
 </style>
